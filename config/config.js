89e5512d6a22851fced7c4b7e91c9f6dc5fb43cf
Rakib Shelah Ahmed <Rakibmedieinstitet@gmail.com>
2020-03-31 10:02:11 +0200

    setting up for heroku

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 //connection string from mongodb.
+// glöm ej npm i --save-dev dotenv   // dotenv läser env variabler
 require('dotenv').config()
+
+//process.env.varnamnet
 
 const config = {
     databaseURL:process.env.DATABASE, 
